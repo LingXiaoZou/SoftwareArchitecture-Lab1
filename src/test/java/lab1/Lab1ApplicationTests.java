4081f9618a8cf2d5c993bb4119276fea17d2450f
zlx <505925146@qq.com>
2023-11-20 16:46:59 +0800

    git add:增加测试

--- src/test/java/lab1/Lab1ApplicationTests.java
+++ src/test/java/lab1/Lab1ApplicationTests.java
@@ -50,4 +50,106 @@ class Lab1ApplicationTests {
         thread2.interrupt(); // 通知线程停止
     }
 
+//以下部分分别对全广播式、选择广播式-点对点、选择广播式-发布订阅三种调度策略进行测试
+
+    /**
+     * 测试点对点
+     * @throws IOException
+     */
+    @Test
+    void TestDirect() throws Exception {
+        //消费者1
+        Consumer cons1 = new Consumer("cons1");
+        Thread thread1 = new Thread(cons1);
+        thread1.start();
+
+        //消费者2
+        Consumer cons2 = new Consumer("cons2");
+        Thread thread2 = new Thread(cons2);
+        thread2.start();
+
+        //生产者1
+        Producer prod1 = new Producer("prod1");
+
+        Thread.sleep(500);
+        //中间件
+        Broker broker = new Broker();
+        broker.subscribe("queue1", cons1.getSocketAddress());
+        broker.subscribe("queue1", cons2.getSocketAddress());
+        broker.start();
+
+        Thread.sleep(500);
+        //生产者产生消息
+        prod1.send("direct", "queue1", "test msg\n");
+
+        thread1.interrupt();
+        thread2.interrupt();
+    }
+
+    /**
+     * 测试广播
+     */
+    @Test
+    void TestFanout() throws Exception {
+        //消费者1
+        Consumer cons1 = new Consumer("cons1");
+        Thread thread1 = new Thread(cons1);
+        thread1.start();
+
+        //消费者2
+        Consumer cons2 = new Consumer("cons2");
+        Thread thread2 = new Thread(cons2);
+        thread2.start();
+
+        //生产者1
+        Producer prod1 = new Producer("prod1");
+
+        Thread.sleep(500);
+        //中间件
+        Broker broker = new Broker();
+        broker.subscribe("queue1", cons1.getSocketAddress());
+        broker.subscribe("queue1", cons2.getSocketAddress());
+        broker.start();
+
+        Thread.sleep(500);
+        //生产者产生消息
+        //这里没有指定队列，依旧可以接收
+        prod1.send("fanout", "", "test msg\n");
+
+        thread1.interrupt();
+        thread2.interrupt();
+    }
+
+    /**
+     * 测试发布/订阅模式
+     */
+    @Test
+    void TestPubsub() throws Exception {
+        //消费者1
+        Consumer cons1 = new Consumer("cons1");
+        Thread thread1 = new Thread(cons1);
+        thread1.start();
+
+        //消费者2
+        Consumer cons2 = new Consumer("cons2");
+        Thread thread2 = new Thread(cons2);
+        thread2.start();
+
+        //生产者1
+        Producer prod1 = new Producer("prod1");
+
+        Thread.sleep(500);
+        //中间件
+        Broker broker = new Broker();
+        broker.subscribe("queue1", cons1.getSocketAddress());
+        broker.subscribe("queue2", cons2.getSocketAddress());
+        broker.start();
+
+        Thread.sleep(500);
+        //生产者产生消息
+        prod1.send("fanout", "queue1", "test msg\n");
+
+        thread1.interrupt();
+        thread2.interrupt();
+    }
 }
