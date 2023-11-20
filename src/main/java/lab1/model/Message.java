9a3360f27a1dc2dc4c6f45f43e165e80d93e3d31
zlx <505925146@qq.com>
2023-11-20 16:46:05 +0800

    git fix:使用lombok简化Message

--- src/main/java/lab1/model/Message.java
+++ src/main/java/lab1/model/Message.java
@@ -1,36 +1,19 @@
 package lab1.model;
 
-import com.google.gson.Gson;
 
+import lombok.Data;
+
+@Data
 public class Message {
     private final String name;
     private final String sendMode;
-    private final String dataType;
+    private final String queueKey;
     private final String data;
 
     public Message(String name, String sendMode, String dataType, String data) {
         this.name = name;
         this.sendMode = sendMode;
-        this.dataType = dataType;
+        this.queueKey = dataType;
         this.data = data;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSendMode() {
-        return sendMode;
-    }
-
-    public String getDataType() {
-        return dataType;
-    }
-
-    public String getData() {
-        return data;
-    }
-
-
-
 }
